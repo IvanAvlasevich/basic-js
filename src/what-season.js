4ee1debbea0e96f8d96c1b56c6d35906faae9168
IvanAvlasevich <avlasevich_ij@yahoo.com>
2023-04-15 11:06:47 +0300

    feat: add new tests done

--- src/what-season.js
+++ src/what-season.js
@@ -11,12 +11,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
+
 function getSeason( date ) {
   if(!date){
     return 'Unable to determine the time of year!'
   }
   if(typeof date == 'number'){throw new Error(`Invalid date!`)}
+  if( !(date instanceof Date) ){
+    throw new Error(`Invalid date!`)
+  }
   if(new Date(date)=='Invalid Date'){
+    throw new Error(`Invalid date!`)
+  }
+  if(date.hasOwnProperty('toString')){
     throw new Error(`Invalid date!`)
   }
   
